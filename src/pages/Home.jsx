7f4c4acda94f76eb1aa1a67962f6a52a49e45ab0
havoclolo <adebowalemarvel@gmail.com>
2026-09-04 20:55:59 +0100

    Add Jewelry hero to home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,7 @@
 import Hero from '../components/Hero'
 import Marketplace from '../components/Marketplace'
 import CategoryHero1 from '../components/CategoryHero1'
+import { JEWELRY_BREADCRUMB } from '../data'
 
 const Home = () => {
   return (
@@ -8,6 +9,11 @@ const Home = () => {
       <Hero/>
       <Marketplace/>
       <CategoryHero1/>
+      <CategoryHero1
+        breadcrumb={JEWELRY_BREADCRUMB}
+        heading="Wholesale Jewelry"
+        heroImage="https://d2njprwt6vp5kv.cloudfront.net/category/background/bg_category_jewelry.png"
+      />
     </div>
   )
 }
